3d353bc464ca52e045c41b81978e0d542899ceab
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2023-10-30 16:48:07 +0100

    Update setup.py
    setup update for version  v0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 REPO_NAME = "ZkIPYNBrenderer"
 AUTHOR_USER_NAME = "Muhammad Zeerak Khan"
